0d384afc8e45919284269710c34dbf17e331843e
Lazar <lakividic01@gmail.com>
2026-01-31 09:48:30 +0100

    Created Repository, Controller and Seed data

---
+++ skinet/Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,28 @@
+
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+
+    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
+
+    Task<Product?> GetProductByIdAsync(int id);
+
+    Task<IReadOnlyList<string>> GetBrandsAsync();
+
+     Task<IReadOnlyList<string>> GetTypesAsync();
+
+    void AddProduct(Product product);
+
+    void UpdateProduct(Product product);
+
+    void DeleteProduct(Product product);
+
+    bool ProductExsists(int id);
+
+    Task<bool> SaveChangesAsync();
+
+
+}
